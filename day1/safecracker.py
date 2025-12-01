cd221c9ed0f0ccc7d335a7f7f6bc4eb04802b0c5
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-01 11:10:24 -0800

    day 1 complete.  part 2 was deceptively tricky.

--- day1/safecracker.py
+++ day1/safecracker.py
@@ -24,17 +24,16 @@ for t in turns:
     full_turns = dist // 100
     zero_pass += full_turns
     dist = dist % 100
-    if current_pos == 0:
-        continue
+    previous_pos = current_pos 
     if t[0] == 'L':
         current_pos -= dist
-        if current_pos < 0:
+        if current_pos < 1 and previous_pos != 0:
             zero_pass += 1
-            current_pos %= 100
+        current_pos %= 100
     else:
         current_pos += dist
-        if current_pos > 99:
+        if current_pos > 99 and previous_pos != 0:
             zero_pass += 1
-            current_pos %= 100
+        current_pos %= 100
     
 print(zero_pass)
