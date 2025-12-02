edd23b78d16cca8d83545e743e057eef86869853
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-02 13:42:49 -0800

    was hoping to avoid regex but oh well

--- day2/doublemint.py
+++ day2/doublemint.py
@@ -1,16 +1,22 @@
+import re
+
 if __name__ == '__main__':
     with open('input') as f:
         ranges = f.readline().split(',')
 
     valid_count = 0
+    valid_count_part2 = 0
+    pattern = re.compile(r'^(\d+)\1+$')
     for r in ranges:
         start,end = map(int, r.split('-'))
         for n in range(start,end+1):
             n_str = str(n)
-            if len(n_str) % 2 == 1:  # numbers with an odd numer of digits can't be valid
-                continue
-            
-            if n_str[:len(n_str)//2] == n_str[len(n_str)//2:]:
-                valid_count += n
+            if re.search(pattern, n_str):
+                valid_count_part2 += n
+            if len(n_str) % 2 == 0:  
+                if n_str[:len(n_str)//2] == n_str[len(n_str)//2:]:
+                    valid_count += n
 
     print(valid_count)
+    print(valid_count_part2)
+
