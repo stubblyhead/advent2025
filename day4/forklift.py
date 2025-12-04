bb08fef8e4e2f2f8963ab3ddf92fac6d633a4f0d
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-04 09:40:56 -0800

    day 4 part 2 complete

--- day4/forklift.py
+++ day4/forklift.py
@@ -9,15 +9,46 @@ def get_adjacent(grid, row, col):
 
     return adjacent
 
-
-if __name__ == '__main__':
-    with open('input') as f:
-        grid = list(map(str.strip, f.readlines()))
-    accessible = 0
+def get_accessible(grid):
+    result = {'accessible_count':0, 'accessible_list':[]}
     for row in range(len(grid)):
         for col in range(len(grid[row])):
+
             if grid[row][col] == '@' and get_adjacent(grid,row,col) < 4:
-                accessible += 1
-    print(accessible)
+                result['accessible_count'] += 1
+                result['accessible_list'].append((row,col))
+    return result
+
+def count_rolls(grid):
+    count = 0
+    for row in grid:
+        for space in row:
+            if space == '@':
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    grid = []
+    with open('input') as f:
+        for r in f.readlines():
+            grid.append(list(r.strip()))
+    accessible = 0
+    roll_count = 0
+    accessible = get_accessible(grid)
+    print(accessible['accessible_count'])
+    
+    roll_count = count_rolls(grid)
+    original_count = roll_count
+    new_count = None
+    while roll_count != new_count:
+        roll_count = new_count
+        accessible = get_accessible(grid)
+        for r,c in accessible['accessible_list']:
+            grid[r][c] = '.'
+        new_count = count_rolls(grid)
+
+    print(original_count - new_count)
+
 
         
