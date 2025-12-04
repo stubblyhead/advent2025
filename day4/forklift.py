fcd3d8d34fefd186833ff4cac0d16fe495d9651c
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-03 23:58:13 -0800

    part 1 done, need to read more carefully

--- day4/forklift.py
+++ day4/forklift.py
@@ -1,7 +1,9 @@
 def get_adjacent(grid, row, col):
     adjacent = 0
-    for r in range(max(0,row-1),min(len(grid),row+1)):
-        for c in range(max(0,col-1),min(len(grid[row]),col+1)):
+    for r in range(max(0,row-1),min(len(grid)-1,row+1)+1):
+        for c in range(max(0,col-1),min(len(grid[row])-1,col+1)+1):
+            if r == row and c == col:
+                continue
             if grid[r][c] == '@':
                 adjacent += 1
 
@@ -9,12 +11,12 @@ def get_adjacent(grid, row, col):
 
 
 if __name__ == '__main__':
-    with open('testcase') as f:
+    with open('input') as f:
         grid = list(map(str.strip, f.readlines()))
     accessible = 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
-            if grid[row][col] == '.' and get_adjacent(grid,row,col) < 4:
+            if grid[row][col] == '@' and get_adjacent(grid,row,col) < 4:
                 accessible += 1
     print(accessible)
 
