64d63858674950cfd43cfdaafe4c8fb7fb282d4a
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-08 21:31:39 -0800

    well I guess i really did just need to transpose it, but there was a lot of string massaging along the way

--- day6/mathhomework.py
+++ day6/mathhomework.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from math import prod
+# from pandas import DataFrame
 
 if __name__ == '__main__':
     with open('input') as f:
@@ -24,3 +25,27 @@ if __name__ == '__main__':
             grand_total += prod(c)
 
     print(grand_total)
+
+    # I think all i really need to do is transpose it?
+    with open('input') as f:
+        stuff = [ list(i.rstrip('\n')) for i in f.readlines() ]
+    tp_stuff = list(map(list,zip(*stuff)))
+    back_to_string = ''
+    for l in tp_stuff:
+        back_to_string += (''.join(l)) + '\n'
+
+    problems = back_to_string.split(' '*len(stuff))
+    grand_total = 0
+    for p in problems:
+        numbers = p.strip().split('\n')
+        operation = numbers[0][-1]
+        numbers[0] = numbers[0][:-1]
+        numbers = [ int(n) for n in numbers ]
+        if operation == '+':
+            grand_total += sum(numbers)
+        else:
+            grand_total += prod(numbers)
+
+    print(grand_total)
+        
+        
