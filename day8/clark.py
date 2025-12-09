5781e998f77bfd10a9694a04b98d89318820f415
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-08 19:18:43 -0800

    this might actually work

--- day8/clark.py
+++ day8/clark.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from itertools import combinations
 
 class Box:
     def __init__(self, x,y,z):
@@ -6,8 +7,8 @@ class Box:
         self.y = y
         self.z = z
 
-    def get_distance(self, other):
-        return sqrt((self.x - other.x)**2 + (self.y - other.y)**2 + (self.z - other.z)**2)
+def get_distance(one, other):
+    return sqrt((one.x - other.x)**2 + (one.y - other.y)**2 + (one.z - other.z)**2)
 
 class Circuit:
     def __init__(self, lights=[]):
@@ -21,3 +22,24 @@ class Circuit:
     
     def get_box_count(self):
         return len(self.boxes)
+    
+if __name__ == '__main__':
+    with open('input') as f:
+        lines = f.readlines()
+
+    boxes = []
+    circuits = []
+    distances = []
+
+    for l in lines:
+        x,y,z = l.split(',')
+        boxes.append(Box(int(x),int(y),int(z)))
+        # honestly a circuits object will prbably be more trouble than it's worth
+        # circuits.append(Circuit([boxes[-1]]))
+        circuits.append([boxes[-1]])
+    
+    for i in combinations(boxes,2):
+        distances.append((get_distance(*i),i))
+    distances.sort()
+
+    print(len(distances))
