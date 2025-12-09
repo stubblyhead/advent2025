98fc1f651202db2dcef578ac7ceb65a5557e1139
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-08 20:27:22 -0800

    removing Circuit class since it wasn't useful

--- day8/clark.py
+++ day8/clark.py
@@ -10,19 +10,7 @@ class Box:
 def get_distance(one, other):
     return sqrt((one.x - other.x)**2 + (one.y - other.y)**2 + (one.z - other.z)**2)
 
-class Circuit:
-    def __init__(self, lights=[]):
-        self.boxes = lights
 
-    def add(self, other):
-        return Circuit(self.lights + other.lights)
-    
-    def contains(self, box):
-        return box in self.boxes
-    
-    def get_box_count(self):
-        return len(self.boxes)
-    
 if __name__ == '__main__':
     with open('input') as f:
         lines = f.readlines()
