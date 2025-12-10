521d9045d3c3223c473ed711efb22989c52b562b
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-10 14:01:42 -0800

    part 1 complete

--- day10/lightsout.py
+++ day10/lightsout.py
@@ -28,6 +28,15 @@ class LightArray:
         for b in btn:
             self.lights[b].toggle()
 
+    def set_lights(self,pattern):
+        pattern = pattern[1:-1]
+        for l in range(len(pattern)):
+            if pattern[l] == '#':
+                self.lights[l].turn_on()
+            else:
+                self.lights[l].turn_off()
+
+
     def reset(self):
         for l in self.lights:
             l.turn_off() 
@@ -44,26 +53,50 @@ class LightArray:
     
     def __eq__(self,s):
         return s == self.__repr__()
+    
+    def __ne__(self,s):
+        return not self == s
 
 
 if __name__ == '__main__':
-    with open('testcase') as f:
+    with open('input') as f:
         lines = f.readlines()
 
-        indicators = []
-        buttons = []
-        joltages = [] # I'm sure we'll need these in part 2
+    targets = []
+    light_arrays = []
+    buttons = []
+    joltages = [] # I'm sure we'll need these in part 2
 
-        for l in lines:
-            ind,rest = l.split('] ')
-            indicators.append(ind+']')
-            button,joltage = rest.split('{')
-            buttons.append(button)
-            joltages.append('{'+joltage)
+    for l in lines:
+        ind,rest = l.split('] ')
+        targets.append(ind+']')
+        light_arrays.append(LightArray(len(targets[-1])-2))
+        button,joltage = rest.split('{')
+        buttons.append(button)
+        joltages.append('{'+joltage.strip())
+    total_presses = 0
+    memo = {} # keeping this in my back pocket
 
-        
+    for line in range(len(lines)):
+        t = targets[line]
+        la = light_arrays[line]
+        b = buttons[line].split()
+        presses = 0
+        found = False
+        while not found:
+            presses += 1
+            for comb in cwr([i for i in range(len(b))], r=presses):
+                la.reset()
+                for press in comb:
+                    la.press_button(b[press])
+                if la == t:
+                    found = True
+                    break
+                else:
+                    memo[comb] = la.__repr__()
+        total_presses += presses
 
-
+    print(total_presses)
 
     
 
